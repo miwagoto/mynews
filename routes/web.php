dcd86a2975b7a8dcf335a9ed26ddf09c5a587085
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2025-01-22 02:39:22 +0000

    カリキュラム13

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('news/create', 'add')->middleware('auth');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
 Route::get('XXX', [AAAController::class, 'bbb']);
