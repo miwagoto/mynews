c75be15257d905fed2d813ba993d97f73cff7ac8
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2025-01-24 03:24:46 +0000

    カリキュラム14

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
-    Route::get('news/create', 'add')->middleware('auth');
+    Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create', 'create')->name('news.create');
 });
 
@@ -27,8 +27,8 @@ Route::get('XXX', [AAAController::class, 'bbb']);
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function (){
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit' ,'edit')->middleware('auth');
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::get('profile/edit' ,'edit')->name('profile.edit');
     Route::post('profile/create', 'create')->name('profile.create');
     Route::post('profile/edit', 'update')->name('profile.update');
 });
