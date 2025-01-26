b05077f06fba9eae35327f348a483e6fab7196d2
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2025-01-26 05:08:00 +0000

    カリキュラム14

--- database/migrations/2025_01_24_013725_create_news_table.php
+++ database/migrations/2025_01_24_013725_create_news_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
+            $table->string('title'); // ニュースのタイトルを保存するカラム
+            $table->string('body');  // ニュースの本文を保存するカラム
+            $table->string('image_path')->nullable();  // 画像のパスを保存するカラム
             $table->timestamps();
         });
     }
