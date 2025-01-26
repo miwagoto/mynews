a4e9c4cbfa9ca3de18736b835c4657f87a5386ec
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2025-01-26 09:04:51 +0000

    課題16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,34 +16,29 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
-        $this->validate($request, News::$rules);
+        $this->validate($request, Profile::$rules);
 
-        $news = new News;
+        $profile = new Profile;
         $form = $request->all();
-
-        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-        if (isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
-        } else {
-            $news->image_path = null;
-        }
 
         // フォームから送信されてきた_tokenを削除する
         unset($form['_token']);
-        // フォームから送信されてきたimageを削除する
-        unset($form['image']);
 
         // データベースに保存する
-        $news->fill($form);
-        $news->save();
+        $profile->fill($form);
+        $profile->save();
         
         return redirect('admin/profile/create');
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
     public function update()
