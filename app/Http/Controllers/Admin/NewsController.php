37a5eab7cf4a220c96055d4fa61b94e56d923252
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2025-01-27 02:57:57 +0000

    カリキュラム17

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 use App\Models\News;
 
+// 以下の2行を追記することで、History Model, Carbonクラスが扱えるようになる
+use App\Models\History;
+use Carbon\Carbon;
+
 class NewsController extends Controller
 {
     public function add()
@@ -91,6 +95,11 @@ class NewsController extends Controller
 
         // 該当するデータを上書きして保存する
         $news->fill($news_form)->save();
+
+        $history = new History();
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
         return redirect('admin/news');
     }
